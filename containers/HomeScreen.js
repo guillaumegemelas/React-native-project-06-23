6213621a21aed7f40cd0f21481f9e0a87cc1a08b
Guillaume Gemelas <guillaumegemelas@hotmail.com>
2023-05-10 08:49:05 +0200

    add routes1

--- containers/HomeScreen.js
+++ containers/HomeScreen.js
@@ -5,7 +5,9 @@ export default function HomeScreen({ navigation }) {
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text>This is the HomeScreen component</Text>
 
-      <TouchableOpacity onPress={() => navigation.navigate("Product")}>
+      <TouchableOpacity
+        onPress={() => navigation.navigate("Product", { poids: "10" })}
+      >
         <Text>Go to product!</Text>
       </TouchableOpacity>
     </View>
--- containers/ProductScreen.js
+++ containers/ProductScreen.js
@@ -1,9 +1,10 @@
 import { Text, View } from "react-native";
 
-const ProductScreen = () => {
+const ProductScreen = ({ route }) => {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text>This is the ProductScreen component</Text>
+      <Text>Poids passé lors de la navigation : {route.params.poids}</Text>
     </View>
   );
 };
